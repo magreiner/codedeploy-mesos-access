c59eed24bebe70fcf4e22022a5329aead8faa5e0
Matthias Greiner <Matthias.Greiner@stud.uni-heidelberg.de>
2016-04-16 12:32:55 +0200

    cleaning

--- scripts/haproxy-external.sh
+++ scripts/haproxy-external.sh
@@ -18,27 +18,6 @@ while [ "$MASTER_INSTANCES_ONLINE" -lt "$MIN_MASTER_INSTANCES" ]; do
 done
 FIRST_MASTER_IP=$(echo "$MASTER_IPS" | head -n1)
 
-#
-# DOCKER
-#
-apt-get update
-apt-get --yes install apt-transport-https ca-certificates
-
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-
-echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
-
-apt-get update
-apt-get --yes install apparmor \
-                      linux-image-extra-$(uname -r)
-apt-get --yes install docker-engine
-
-usermod -aG docker ubuntu
-
-service docker start
-
-
-
 # start external haproxy
 docker kill haproxy-external &>/dev/null
 docker rm haproxy-external &>/dev/null
