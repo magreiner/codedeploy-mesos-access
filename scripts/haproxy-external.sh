5f787b920ea9dcfced69483b18e89203b128a26e
Matthias Greiner <Matthias.Greiner@stud.uni-heidelberg.de>
2016-05-17 22:43:24 +0200

    added dns services

--- scripts/haproxy-external.sh
+++ scripts/haproxy-external.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-MIN_MASTER_INSTANCES=1
-MASTER_INSTANCE_TAGNAME="AS_Master"
-
-AZ="$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)"
-REGION="${AZ::-1}"
-
-# Looking for other master instances for HA (Zookeeper)
-MASTER_IPS=$(aws ec2 describe-instances --region $REGION --filters "Name=tag:Name,Values=$MASTER_INSTANCE_TAGNAME" | jq '. | {ips: .Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddress}' | grep "\." | cut -f4 -d'"')
-MASTER_INSTANCES_ONLINE=$(echo "$MASTER_IPS" | grep "\." | wc -l)
-
-while [ "$MASTER_INSTANCES_ONLINE" -lt "$MIN_MASTER_INSTANCES" ]; do
-  sleep 2
-  echo "Waiting for more master instances. ($MASTER_INSTANCES_ONLINE/$MIN_MASTER_INSTANCES online)"
-  MASTER_IPS=$(aws ec2 describe-instances --region $REGION --filters "Name=tag:Name,Values=$MASTER_INSTANCE_TAGNAME" | jq '. | {ips: .Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddress}' | grep "\." | cut -f4 -d'"' | head -n1)
-  MASTER_INSTANCES_ONLINE=$(echo "$MASTER_IPS" | grep "\." | wc -l)
-done
-FIRST_MASTER_IP=$(echo "$MASTER_IPS" | head -n1)
-
 # start external haproxy
 docker kill haproxy-external &>/dev/null
 docker rm haproxy-external &>/dev/null
@@ -26,7 +8,7 @@ docker run --name haproxy-external \
            -d \
            -e PORTS=1000 \
            --net=host mesosphere/marathon-lb sse \
-           -m http://$FIRST_MASTER_IP:8080 \
+           -m http://leader.mesos:8080 \
            --dont-bind-http-https \
            --group "ext"
 
